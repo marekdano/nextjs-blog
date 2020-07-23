57960e94b26b8ae23791a3df43fe4e8e00702cb5
Marek Dano <mk.dano@gmail.com>
2020-07-23 23:28:51 +0100

    Replace Button component with Counter

--- components/Counter.tsx
+++ components/Counter.tsx
@@ -1,5 +1,9 @@
-export default function Button({ children }) {
+import React, { useState } from 'react'
+
+export default function Counter({ children }) {
+  const [count, setCount] = useState(0)
   return (
+    
     <button
       style={{
         borderRadius: '3px',
@@ -8,9 +12,11 @@ export default function Button({ children }) {
         padding: '0.5em 1em',
         cursor: 'pointer',
         fontSize: '1.1em',
+        width: '10rem',
       }}
+      onClick={() => setCount(count + 1)}
     >
-      {children}
+      Counter: {count}
     </button>
   )
 }
