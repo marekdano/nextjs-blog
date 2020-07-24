e74af03fd2ce5307bccf6e64c1e4abf6e10abad7
Marek Dano <mk.dano@gmail.com>
2020-07-24 19:06:03 +0100

    Style and disable hidden input in ContactForm

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,10 +1,27 @@
 export default function ContactForm() {
-  return <form name="contact" action="/success" method="POST" data-netlify="true">
-    <input type="hidden" name="form-name" value="contact" />
-    <p>
-      <label htmlFor="your-email">Email: </label>
-      <input type="email" id="your-email" />
-    </p>
-    <button type="submit">Submit</button>
-  </form>
+  return (
+    <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" name="contact" action="/success" method="POST" data-netlify="true">
+      {/* <input type="hidden" name="form-name" value="contact" /> */}
+      <div className="mb-4">
+        <label 
+          className="block text-gray-700 text-sm font-bold mb-2" 
+          htmlFor="your-email"
+        >
+          Email
+        </label>
+        <input 
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+          type="email" 
+          id="your-email" 
+          placeholder="Email"
+        />
+      </div>
+      <button 
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        type="submit"
+      >
+        Submit
+      </button>
+    </form>
+  )
 }
